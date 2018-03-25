d99136165223262fad08424896685ecffef0f6f3
Joosang Lee <armlessarcher@gmail.com>
2018-03-25 15:19:28 -0700

    add rerouting based on selection

--- src/ReportPage.js
+++ src/ReportPage.js
@@ -2,26 +2,42 @@ import React from "react";
 import * as yes from "./yes.png";
 import * as no from "./no.png";
 import request from 'request';
+import {Redirect} from 'react-router-dom'
+
 
 class ReportPage extends React.Component {
     constructor(props) {
         super(props);
     }
     state = {
-        yesOrNo: null
+        yesOrNo: null,
+        response:null
     }
 
     yes() {
         console.log("yes");
-        this.setState({yesOrNo:true});
-       
+        this.sendAndWaitForResponse(true);
+
     }
 
     no() {
-       
+       console.log("no");
+        this.setState({response:false});
     }
+    //placeholder until endpoint
+    sendAndWaitForResponse(yesOrNo){
+        
+        this.setState({response:true});
+    }
+
+
     
     render() {
+        if(this.state.response===true){
+            return <Redirect push to="/chat" />;
+        } else if (this.state.response===false){
+            return <Redirect push to="/" />;
+        }
         return(
             <div className="centre-box">
                 <div className="question">How was it?</div>
