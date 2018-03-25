a2ff2e66eadf6522f732f6036421c642bac33e7c
Joosang Lee <armlessarcher@gmail.com>
2018-03-25 13:30:55 -0700

    images clickable

--- src/ReportPage.js
+++ src/ReportPage.js
@@ -1,26 +1,45 @@
 import React from "react";
 import * as yes from "./yes.png";
 import * as no from "./no.png";
+import axios from "axios";
 
 class ReportPage extends React.Component {
     constructor(props) {
         super(props);
     }
+    state = {
+        yesOrNo: null
+    }
     yes() {
-
+        console.log("yes");
+        this.setState({yesOrNo:true});
     }
 
     no() {
-
+        console.log("no");
+        this.setState({yesOrNo:false});
     }
+    
+    componentDidUpdate() {
+        if(this.state.yesOrNo){
+            //send to chat
+        } else {
+            //send to home
+        }
+    }
+    
 
-    render(){
+    render() {
         return(
             <div className="centre-box">
                 <div className="question">How was it?</div>
-                <div>
-                    <img src={yes} className="img-select" onClick={this.yes}/><span>Yes? </span>
-                    <img src={no} className="img-select" onClick={this.no}/><span>No? </span>
+                <div className="inner-box">
+                    <span className="img-box">
+                        <img src={yes} className="img-select" id="yes" onClick={this.yes.bind(this)}/>
+                        </span>
+                    <span className="img-box">
+                        <img src={no} className="img-select" id="no" onClick={this.no.bind(this)}/>
+                    </span>
                 </div>
             </div>
         );
