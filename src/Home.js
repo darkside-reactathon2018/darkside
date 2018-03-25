1b324948a4aa993b68c016a94c42c6e9518074db
Wendy <tell.wendy@gmail.com>
2018-03-25 00:12:57 -0700

    add logo and style to nav create buttons and links

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  margin: 100px;
+`;
+
+const Button = styled.span`
+  background-color: var(--blue);
+  color: #fff;
+  font-size: 20px;
+  border-radius: 15px;
+  padding: 10px;
+
+  &:hover {
+    box-shadow: 0 2px 10px var(--pink);
+    
+  }
+`;
+
+const LoginLink = styled.span`
+  color: var(--dark-grey);
+  padding: 10px;
+
+  &:hover {
+    color: var(--pink);
+  }
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Home = props => 
-  <div>
-    Landing Page
-  </div>
+  <Container>
+    <LinkContainer>
+    <Link to="/create" style={{margin: "10px"}}><Button>Create a Profile</Button></Link>
+  {/* TODO: currently go directly to dummy user profile, build log in page if time allows */}
+    <Link to="/user/1" style={{margin: "10px"}}><LoginLink>or Log in</LoginLink></Link>
+    </LinkContainer>
+  </Container>
 
 export default Home;
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import * as logo from "./logo.png";
 
 const Nav = props => 
   <div>
-    <nav>
-      this is the nav
+    <nav style={{boxShadow: "0 5px 5px -4px gray"}}>
+      <Link to="/"><img style={{ maxWidth: "200px"}} src={logo} /></Link>
     </nav>
     {props.children}
   </div>
