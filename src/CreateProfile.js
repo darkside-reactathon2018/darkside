a6e108684b997ee9fcdfcba436097bc2c9c5a92d
ClarabelleCheng-Yue <cachengyue@gmail.com>
2018-03-25 11:08:51 -0700

    Make profile questions

--- src/CreateProfile.js
+++ src/CreateProfile.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
+import * as style from './CreateProfile.css';
 
 class CreateProfile extends Component {
   state = {
 
   }
 
+  renderQuestion(labelText, id, placeholder) {
+    return (
+      <div>
+      <label for={id}>{labelText}</label>
+      <input 
+        type='text'
+        id={id}
+        name={id}
+        placeholder={placeholder}
+      />
+      </div>
+    );
+  }
+
   render() {
     return(
-      <div>
-        Create Profile
+      <div style={style}>
+        <form>
+          {
+            this.renderQuestion('First Name', 'fname', 'First name')
+          }
+          {
+            this.renderQuestion('Last Name', 'lname', 'Last name')
+          }
+          {
+            this.renderQuestion('Describe yourself', 'describe', "Don't sound like a creep.")
+          }
+          {
+            this.renderQuestion('Hobbies', 'hobbies', 'What do you do for fun?')
+          }
+          {
+            this.renderQuestion('Profession', 'work', 'What do you do for money?')
+          }
+        </form>
       </div>
       );
   }
